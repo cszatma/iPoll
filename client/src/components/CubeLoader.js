ca8cc7fe508cf52dca5352d83269e8d791ac9141
cszatma <christopherszatmary@gmail.com>
2018-03-17 16:41:21 -0400

    Added SpinKit package, added CubeLoader component

---
+++ client/src/components/CubeLoader.js
@@ -0,0 +1,22 @@
+// @flow
+
+import React from 'react';
+
+import 'spinkit/css/spinners/11-folding-cube.css';
+
+const CubeLoader = () => {
+    const cubes = [1, 2, 4, 3].map(i => (
+        <div
+            key={i}
+            className={`sk-cube sk-cube${i}`}
+        />
+    ));
+
+    return (
+        <div className="sk-folding-cube">
+            {cubes}
+        </div>
+    );
+};
+
+export default CubeLoader;
