6f157925cc157fe9a9023e56778330c718959dae
Christopher Szatmary <christopherszatmary@gmail.com>
2018-03-16 22:29:21 -0400

    Rebuilt root level component using Provider and ConnectedRouter to setup the redux store and router

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,28 @@
+// @flow
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
+import { Route, IndexRoute } from 'react-router';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/index.scss';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './components/App';
+import store, { history } from './store';
+
+
+const router = (
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <Route path="/" component={App}>
+            </Route>
+        </ConnectedRouter>
+    </Provider>
+);
+
+// $FlowExpectedError
+ReactDOM.render(router, document.getElementById('root'));
 registerServiceWorker();
