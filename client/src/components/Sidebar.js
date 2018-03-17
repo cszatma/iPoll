4a431ca814a14f3d7feda812db1d98f729cfceba
Christopher Szatmary <christopherszatmary@gmail.com>
2018-03-16 22:34:09 -0400

    Added Sidebar component which will allow navigation to various parts of the app

---
+++ client/src/components/Sidebar.js
@@ -0,0 +1,19 @@
+// @flow
+
+import React from 'react';
+import { Nav } from 'reactstrap';
+
+/*
+Dashboard
+Courses
+Account
+Logout
+ */
+
+const Sidebar = () => (
+    <Nav className="flex-column">
+
+    </Nav>
+);
+
+export default Sidebar;
