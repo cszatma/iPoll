ab6979a6618be84403a011cdf09624fe0a7209af
cszatma <christopherszatmary@gmail.com>
2018-03-17 17:58:24 -0400

    Added Form component which can be used to generate forms with text inputs

---
+++ client/src/components/Form.js
@@ -0,0 +1,106 @@
+// @flow
+
+import React, { Component } from 'react';
+import { Button, Form as RSForm, FormGroup, FormFeedback, Label } from 'reactstrap';
+
+import type { InputType, InputError, InputData } from '../types';
+import Input from './Input';
+import { capitalizeString } from '../utils';
+
+type Props = {
+    inputs: InputType[],
+    onSubmit: InputData[] => void,
+    submitText: string,
+};
+
+type State = {
+    inputs: { +type: InputType, value: string }[],
+    inputErrors: InputError[],
+};
+
+export default class Form extends Component<Props, State> {
+    state = {
+        inputs: this.props.inputs.map(input => ({ type: input, value: '' })),
+        inputErrors: this.props.inputs.map(input => ({ input, error: null })),
+    };
+
+    onInputChange = (inputType: InputType, value: string, error: ?string) => {
+        const inputs = this.state.inputs;
+        const inputErrors = this.state.inputErrors;
+
+        const index = inputs.findIndex(input => input.type === inputType);
+
+        if (index === -1) {
+            throw Error('Input does not exist!');
+        }
+
+        this.setState({
+            inputs: [
+                ...inputs.slice(0, index),
+                { type: inputType, value },
+                ...inputs.slice(index + 1)
+            ],
+            inputErrors: [
+                ...inputErrors.slice(0, index),
+                { input: inputType, error },
+                ...inputErrors.slice(index + 1),
+            ],
+        });
+    };
+
+    handleFormSubmit = (event: SyntheticEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        const inputs = this.state.inputs;
+
+        // Check if any inputs are missing
+        const missingInputs = inputs.map(input => !input.value);
+
+        if (missingInputs.some(val => val)) {
+            // Create a new array of errors and update the state
+            const newErrors = this.state.inputErrors.map((error, i) =>
+                missingInputs[i] ? {
+                    input: error.input,
+                    error: inputErrorMessage(error.input.name)
+            } : error);
+
+            this.setState({ inputErrors: newErrors });
+            return;
+        }
+
+        const data = inputs.map(input => ({ name: input.type.name, value: input.value }));
+        this.props.onSubmit(data);
+    };
+
+    render() {
+        const inputs = this.state.inputs.map((input, i) => {
+            const name = input.type.name;
+            const error = this.state.inputErrors[i].error;
+
+            return (
+                <FormGroup key={i}>
+                    <Label for={name}>{capitalizeString(name)}</Label>
+                    <Input
+                        type={input.type}
+                        value={input.value}
+                        validate={value => value.trim() ? null : inputErrorMessage(name)}
+                        onChange={this.onInputChange}
+                        invalid={!!error}
+                    />
+                    {error ? <FormFeedback>{error}</FormFeedback> : null}
+                </FormGroup>
+            );
+        });
+
+        return (
+            <RSForm onSubmit={this.handleFormSubmit}>
+                {inputs}
+                <Button>{this.props.submitText}</Button>
+            </RSForm>
+        );
+    }
+}
+
+function inputErrorMessage(name: string): string {
+    return `${capitalizeString(name)} required.`;
+}
