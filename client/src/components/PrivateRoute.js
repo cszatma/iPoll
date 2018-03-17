3fad3953a9a29aa5cd9db5211ec8a872f0458ec1
cszatma <christopherszatmary@gmail.com>
2018-03-17 14:40:12 -0400

    Added PrivateRoute component which redirects the user to /login if they are not logged in

---
+++ client/src/components/PrivateRoute.js
@@ -0,0 +1,20 @@
+// @flow
+
+import React from 'react';
+import type { ComponentType } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+import client from '../Client';
+
+type Props = {
+    component: ComponentType<any>;
+};
+
+const PrivateRoute = ({ component, ...rest }: Props) => (
+    <Route {...rest} render={props => (
+        client.isLoggedIn() ? React.createElement(component, props) :
+            <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
+    )} />
+);
+
+export default PrivateRoute;
