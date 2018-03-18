f77ecb0be31ef3723a588e359f42f904b50ec92c
cszatma <christopherszatmary@gmail.com>
2018-03-18 17:07:28 -0400

    Wrapped the Routes inside a Switch in App, this prevents / from being matched to /login, etc

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,19 +1,23 @@
 // @flow
 
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import '../styles/App.scss';
 import Login from './Login';
 import Logout from './Logout';
+import Register from './Register';
 import PrivateRoute from './PrivateRoute';
 import AppContainer from './AppContainer';
 
 const App = () => (
     <div>
-        <PrivateRoute exact path="/" component={AppContainer} />
-        <Route path="/login" component={Login} />
-        <Route path="/logout" component={Logout} />
+        <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/logout" component={Logout} />
+            <Route path="/register" component={Register} />
+            <PrivateRoute path="/" component={AppContainer} />
+        </Switch>
     </div>
 );
 
