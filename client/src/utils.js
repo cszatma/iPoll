23c2c3be9410b6629fc2195804ba7e9793830389
cszatma <christopherszatmary@gmail.com>
2018-03-18 13:36:35 -0400

    Added functions to remove dashes and underscores from a string and to capitalize all words in the string

--- client/src/utils.js
+++ client/src/utils.js
@@ -4,4 +4,16 @@ function capitalizeString(str: string): string {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-export { capitalizeString };
+function removeDashesAndUnderscores(str: string): string {
+    return str.replace(/[-_]/g, ' ');
+}
+
+function removeAndCapitalizeAll(str: string): string {
+    return removeDashesAndUnderscores(str).split(' ').map(s => capitalizeString(s)).join(' ');
+}
+
+export {
+    capitalizeString,
+    removeDashesAndUnderscores,
+    removeAndCapitalizeAll,
+};
