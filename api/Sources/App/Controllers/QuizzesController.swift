64596d0144d8fb385ce687a859a5e98c8fa339ff
Chris-Bishop <christopher.allan.bishop@gmail.com>
2018-03-19 06:03:12 -0400

    Work on Quiz Controller

--- api/Sources/App/Controllers/QuizzesController.swift
+++ api/Sources/App/Controllers/QuizzesController.swift
@@ -11,17 +11,28 @@ struct QuizzesController: RouteCollection {
 
         quizzesRoute.get(use: getAllHandler)
         quizzesRoute.post("for-course", Course.parameter, use:createHandler)
+        quizzesRoute.put(Quiz.parameter, use: updateHandler)
+
 
     }
 
-
+    // WORKS
     func getAllHandler(_ req: Request) throws -> Future<[Quiz]> {
         return Quiz.query(on: req).all()
     }
 
+    // WORKS
     func createHandler(_ req: Request) throws -> Future<Quiz> {
         return try flatMap(to: Quiz.self, req.parameter(Course.self), req.content.decode(QuizCreateData.self)) { course, quizData in
             let quiz = try Quiz(courseID: course.id!, title: quizData.title)
+            return quiz.save(on: req)
+        }
+    }
+
+    // To be Tested
+    func updateHandler(_ req: Request) throws -> Future<Quiz> {
+        return try flatMap(to: Quiz.self, req.parameter(Quiz.self), req.content.decode(QuizCreateData.self)) { quiz, updatedQuiz in
+            quiz.title = updatedQuiz.title
             return quiz.save(on: req)
         }
     }
