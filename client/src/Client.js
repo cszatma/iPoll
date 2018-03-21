5129a233b192689e3db387f88803db9e2c34e6f7
Christopher Szatmary <christopherszatmary@gmail.com>
2018-03-21 11:10:56 -0400

    Added getCourses() method to Client to get all courses of a given type, changed type of token from string to Token

--- client/src/Client.js
+++ client/src/Client.js
@@ -1,6 +1,7 @@
 // @flow
 
 import { defaultHeaders, authorizedHeader, apiRequest, parsedApiRequest } from './utils/request';
+import type { Course, CourseType, Token } from './utils/types';
 
 // Global constants
 type CallbackFunction = boolean => void;
@@ -8,12 +9,13 @@ const LOCAL_TOKEN_KEY = 'ipoll-api-token-auth';
 const storage = localStorage; // Use either session or local storage
 
 class Client {
-    token: ?string;
+    token: ?Token;
     subscribers: CallbackFunction[];
 
     constructor() {
         // Set the auth token if it exists
-        this.token = storage.getItem(LOCAL_TOKEN_KEY);
+        const tokenJson = storage.getItem(LOCAL_TOKEN_KEY);
+        this.token = tokenJson ? JSON.parse(tokenJson) : null;
         this.subscribers = [];
 
         // Check that it's a valid token
@@ -31,12 +33,12 @@ class Client {
         return !!this.token;
     }
 
-    setToken(token: string): void {
+    setToken(token: Token) {
         this.token = token;
-        storage.setItem(LOCAL_TOKEN_KEY, token);
+        storage.setItem(LOCAL_TOKEN_KEY, JSON.stringify(token));
     }
 
-    removeToken(): void {
+    removeToken() {
         this.token = null;
         storage.removeItem(LOCAL_TOKEN_KEY);
     }
@@ -49,7 +51,7 @@ class Client {
         }
 
         const isValid = await parsedApiRequest(
-            `tokens/check-token?token=${encodeURIComponent(token)}`,
+            `tokens/check-token?token=${encodeURIComponent(token.token)}`,
             'get',
             defaultHeaders
         );
@@ -57,17 +59,17 @@ class Client {
         return isValid === 1;
     }
 
-    register(username: string, school: string, password: string): Promise<any> {
+    register(username: string, school: string, password: string): Promise<void> {
         const json = JSON.stringify({ username, school, password });
         return apiRequest('users', 'post', defaultHeaders, json)
             .then(() => this.login(username, password));
     }
 
-    login(username: string, password: string): Promise<any> {
+    login(username: string, password: string): Promise<void> {
         return parsedApiRequest('users/login', 'post', {
             ...defaultHeaders,
             'Authorization': 'Basic ' + btoa(`${username}:${password}`),
-        }).then(json => this.setToken(json.token));
+        }).then(json => this.setToken(json));
     }
 
     logout(): Promise<void> {
@@ -78,15 +80,29 @@ class Client {
         }
 
         this.removeToken();
-        return apiRequest(`tokens?token=${token}`, 'delete', defaultHeaders);
+        return apiRequest(`tokens?token=${token.token}`, 'delete', defaultHeaders);
     }
 
-    subscribe(callback: CallbackFunction): void {
+    subscribe(callback: CallbackFunction) {
         this.subscribers.push(callback);
     }
 
     notifySubscribers() {
-        this.subscribers.forEach(callback => callback(this.isLoggedIn()));
+        this.subscribers.forEach(callback =>
+            callback(this.isLoggedIn()));
+    }
+
+    getCourses(type: CourseType): Promise<Course[]> {
+        const token = this.token;
+
+        if (!token) {
+            throw Error('User is not authenticated.');
+        }
+
+        const route = type === 'enrolled' ? 'enrolled' : 'owned-courses';
+        const url = `users/${route}/${token.userID}`;
+
+        return parsedApiRequest(url, 'get', defaultHeaders);
     }
 }
 
