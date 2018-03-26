ef86e1723626243de2607cfdd85dd2d706034566
Christopher Szatmary <christopherszatmary@gmail.com>
2018-03-26 14:54:38 -0400

    Added setCourses to actionCreator, changed the type property of each action to the corresponding string literal from actionTypes

--- client/src/utils/actionCreators.js
+++ client/src/utils/actionCreators.js
@@ -2,10 +2,12 @@
 
 import type { Course } from './types';
 
-type AddCourseAction = { type: string, course: Course };
+type AddCourseAction = { type: 'ADD_COURSE', course: Course };
+type SetCoursesAction = { type: 'SET_COURSES', courses: Course[] };
 
 export const actionTypes = {
     addCourse: 'ADD_COURSE',
+    setCourses: 'SET_COURSES',
 };
 
 /* Action Functions */
@@ -15,13 +17,21 @@ function addCourse(course: Course): AddCourseAction {
         course,
     };
 }
+
+function setCourses(courses: Course[]): SetCoursesAction {
+    return {
+        type: actionTypes.setCourses,
+        courses,
+    };
+}
 /* End Action Functions */
 
 // Exports
 
 const actionCreators = {
     addCourse,
+    setCourses,
 };
 
-export type Action = AddCourseAction;
+export type Action = AddCourseAction | SetCoursesAction;
 export default actionCreators;
