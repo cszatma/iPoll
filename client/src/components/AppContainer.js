274244f68d9e22793fba0e60897c26ba57373cef
Christopher Szatmary <christopherszatmary@gmail.com>
2018-03-26 21:26:58 -0400

    Connected AppContainer to the redux store, added route for individual courses, courses are now fetched when the app loads and stored in the redux store for quick access

--- client/src/components/AppContainer.js
+++ client/src/components/AppContainer.js
@@ -1,12 +1,27 @@
 import React, { PureComponent } from 'react';
 import { Container, Row, Col } from 'reactstrap';
-import { Route, Redirect } from 'react-router-dom';
+import { Route, Redirect, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
+import type { Dispatch } from 'redux';
 
+import NotFound from './NotFound';
 import Sidebar from './Sidebar';
 import Dashboard from './Dashboard';
 import Courses from './courses/Courses';
 import CreateCourse from './courses/CreateCourse';
+import CourseDetails from './courses/CourseDetails';
+import CubeLoader from './CubeLoader';
 import client from '../Client';
+import actionCreators from '../utils/actionCreators';
+
+type Props = {
+    courses: Course[],
+    setCourses: Course[] => void,
+};
+
+type State = {
+    fetchInProgress: boolean,
+};
 
 const sidebarItems = [
     { name: 'Dashboard', url: '/dashboard' },
@@ -15,29 +30,83 @@ const sidebarItems = [
     { name: 'Logout', url: '/logout' },
 ];
 
-export default class AppContainer extends PureComponent<{}> {
+class AppContainer extends PureComponent<Props,State> {
+    state = {
+        fetchInProgress: false,
+    };
+    
     constructor(props: {}) {
         super(props);
         client.subscribe(isValid => !isValid && this.forceUpdate());
     }
 
-   render() {
-       if (!client.isLoggedIn()) {
-           return <Redirect to="/login" />;
-       }
+    async componentWillMount() {
+        // If logged in fetch courses and add them to the redux store for quick access
+        if (client.isLoggedIn()) {
+            this.setState({ fetchInProgress: true });
 
-       return (
-           <Container fluid>
-               <Row>
-                   <Sidebar items={sidebarItems}/>
-                   <Col md="9" className="mt-3">
-                       <Route exact path="/" render={() => <Redirect to="/dashboard" />}/>
-                       <Route path="/dashboard" component={Dashboard} />
-                       <Route exact path="/courses" component={Courses} />
-                       <Route path="/courses/create" component={CreateCourse} />
-                   </Col>
-               </Row>
-           </Container>
-       )
-   }
+            const ownedCourses = await client.getCourses('owned');
+            const enrolledCourses = await client.getCourses('enrolled');
+
+            this.props.setCourses(ownedCourses, 'owned');
+            this.props.setCourses(enrolledCourses, 'enrolled');
+            this.setState({ fetchInProgress: false });
+        }
+    }
+
+    findCourse(id: number): Course {
+        const { ownedCourses, enrolledCourses } = this.props.courses;
+        return [...ownedCourses, ...enrolledCourses].find(course => course.id === id);
+    }
+
+    render() {
+        if (!client.isLoggedIn()) {
+            return <Redirect to="/login" />;
+        } else if (this.state.fetchInProgress) {
+            return <CubeLoader />;
+        }
+
+        const { ownedCourses, enrolledCourses } = this.props.courses;
+
+        return (
+            <Container fluid>
+                <Row>
+                    <Sidebar items={sidebarItems}/>
+                        <Col md="9" className="mt-3">
+                            <Switch>
+                                <Route exact path="/" render={() => <Redirect to="/dashboard" />}/>
+                                <Route path="/dashboard" component={Dashboard} />
+                                <Route exact path="/courses" render={(routeProps) => (
+                                    <Courses
+                                        {...routeProps}
+                                        ownedCourses={ownedCourses}
+                                        enrolledCourses={enrolledCourses}
+                                    />
+                                )}/>
+                                <Route path="/courses/create" component={CreateCourse} />
+                                <Route path="/courses/:courseId" render={({ match }) => {
+                                    const course = this.findCourse(parseInt(match.params.courseId, 10));
+                                    return <CourseDetails course={course} />;
+                                }} />
+                                <Route component={NotFound} />
+                            </Switch>
+                        </Col>
+                </Row>
+            </Container>
+        )
+    }
 }
+
+function mapStateToProps(state: State): Props {
+    return {
+        courses: state.courses,
+    };
+}
+
+function mapDispatchToProps(dispatch: Dispatch) {
+    return {
+        setCourses: (courses, courseType) => dispatch(actionCreators.setCourses(courses, courseType)),
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AppContainer);
