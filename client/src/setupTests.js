e0bb3453c5cc45bb7b4d998ada2d891046d07246
cszatma <christopherszatmary@gmail.com>
2018-04-05 23:39:58 -0400

    Setup mock localStorage for testing

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -3,3 +3,11 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+
+const localStorageMock = {
+    getItem: jest.fn(),
+    setItem: jest.fn(),
+    clear: jest.fn(),
+};
+
+global.localStorage = localStorageMock;
